bd243834d4ea7e2304443d87f469b38be127dd62
verycosy <verycosy@kakao.com>
2021-09-18 22:00:30 +0900

    Initial Commit

---
+++ src/test/java/com/springcart/SpringCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

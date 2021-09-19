db7133c5a93c76a2f5dd338fee49ef1e1266cce3
verycosy <verycosy@kakao.com>
2021-09-19 23:51:43 +0900

    sign up

---
+++ src/main/java/com/springcart/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.springcart.repository;
+
+import com.springcart.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}

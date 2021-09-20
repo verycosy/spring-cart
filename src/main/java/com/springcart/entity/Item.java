49db53c96109788a53416bc2200364fa0829d118
verycosy <verycosy@kakao.com>
2021-09-20 22:46:16 +0900

    auditing

--- src/main/java/com/springcart/entity/Item.java
+++ src/main/java/com/springcart/entity/Item.java
@@ -6,14 +6,13 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "item")
 @Getter
 @Setter
 @ToString
-public class Item {
+public class Item extends BaseEntity {
     @Id
     @Column(name="item_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,7 +33,4 @@ public class Item {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
-
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
 }
--- src/main/java/com/springcart/entity/OrderItem.java
+++ src/main/java/com/springcart/entity/OrderItem.java
@@ -4,11 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
     @Id
     @GeneratedValue
     @Column(name = "order_item_id")
@@ -24,6 +23,4 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
-    private LocalDateTime regTime;
-    private LocalDateTime updateTime;
 }

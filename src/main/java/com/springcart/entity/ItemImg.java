2080549e17514837524c1d9a93e6c45dae153e6a
verycosy <verycosy@kakao.com>
2021-09-20 22:57:49 +0900

    item image entity

---
+++ src/main/java/com/springcart/entity/ItemImg.java
@@ -0,0 +1,31 @@
+package com.springcart.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="item_img")
+@Getter @Setter
+public class ItemImg extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="item_img_id")
+    private Long id;
+
+    private String imgName;
+    private String oriImgName;
+    private String imgUrl;
+    private String repimgYn;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl) {
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+}

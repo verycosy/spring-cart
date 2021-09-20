d6aff431dd43da0e624a64d3460967e9220798fe
verycosy <verycosy@kakao.com>
2021-09-21 01:20:53 +0900

    update item

--- src/main/java/com/springcart/entity/Item.java
+++ src/main/java/com/springcart/entity/Item.java
@@ -1,6 +1,7 @@
 package com.springcart.entity;
 
 import com.springcart.constant.ItemSellStatus;
+import com.springcart.dto.ItemFormDto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -33,4 +34,12 @@ public class Item extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private ItemSellStatus itemSellStatus;
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.itemName = itemFormDto.getItemName();
+        this.price = itemFormDto.getPrice();
+        this.stockNumber = itemFormDto.getStockNumber();
+        this.itemDetail = itemFormDto.getItemDetail();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 }

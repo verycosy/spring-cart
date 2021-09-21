7055d363b3d7b010c9b9a5bf44a431338a9795b8
verycosy <verycosy@kakao.com>
2021-09-22 02:49:15 +0900

    bug fix

--- src/main/java/com/springcart/repository/CartItemRepository.java
+++ src/main/java/com/springcart/repository/CartItemRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartIdAndItemId(Long cartId, Long itemId);
-    @Query("select new com.shop.dto.CartDetailDto(ci.id, i.itemName, i.price, ci.count, im.imgUrl) " +
+    @Query("select new com.springcart.dto.CartDetailDto(ci.id, i.itemName, i.price, ci.count, im.imgUrl) " +
             "from CartItem ci, ItemImg im " +
             "join ci.item i " +
             "where ci.cart.id = :cartId " +
